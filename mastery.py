a5017dfdb243f137d766c53f78c81ffca857cc22
Sadteeto <87579232+Sadteeto@users.noreply.github.com>
2021-11-16 18:52:18 +1000

    Add files via upload

---
+++ mastery.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from tkinter import ttk
+#download webpage and save 
+from urllib.request import urlopen
+# regex
+from re import findall
+key ='RGAPI-aacc5754-3b2a-47ec-81f6-a1a1d4a4a7a8'
+summoner = 'nKodta5l2_IqJpVOatbcT4SuBdvMIFAi704o_VXJP19ftCY?'
+# create main window
+root = Tk()
+root.title("Teemo")
+def enquire():
+    teemo = urlopen(f'https://oc1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{summoner}api_key={key}', timeout=5)
+    # find string
+    masterypoint = findall("championId\":17.+?ints\":([0-9]+)",teemo.read().decode('utf-8'))
+    #update label
+    masterypoint = f'My Teemo mastery is {masterypoint[0]}'
+    print (masterypoint)
+    message.config(text=masterypoint)
+
+message = ttk.Label(root, text="here will displlay your champion mastery")
+message.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+
+Button = ttk.Button(root, text="Click Me!", command=enquire)
+Button.grid(row=1, column=0, padx=10, pady=10, sticky=N)
+#mainloop
+root.mainloop()
