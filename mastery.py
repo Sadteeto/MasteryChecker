370e65a854afcb64604a1b9e728534d0d53206cb
Sadteeto <87579232+Sadteeto@users.noreply.github.com>
2021-12-26 02:24:26 +1000

    Interfaces with arduino hooked up to an lcd

--- mastery.py
+++ mastery.py
@@ -1,27 +1,91 @@
-from tkinter import *
-from tkinter import ttk
-#download webpage and save 
+import serial
+from time import sleep
+from datetime import datetime
 from urllib.request import urlopen
-# regex
 from re import findall
-key ='your API KEY'
-summoner = 'nKodta5l2_IqJpVOatbcT4SuBdvMIFAi704o_VXJP19ftCY?'
-# create main window
-root = Tk()
-root.title("Teemo")
-def enquire():
-    teemo = urlopen(f'https://oc1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{summoner}api_key={key}', timeout=5)
-    # find string
-    masterypoint = findall("championId\":17.+?ints\":([0-9]+)",teemo.read().decode('utf-8'))
-    #update label
-    masterypoint = f'My Teemo mastery is {masterypoint[0]}'
-    print (masterypoint)
-    message.config(text=masterypoint)
 
-message = ttk.Label(root, text="here will displlay your champion mastery")
-message.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
-Button = ttk.Button(root, text="Click Me!", command=enquire)
-Button.grid(row=1, column=0, padx=10, pady=10, sticky=N)
-#mainloop
-root.mainloop()
+key = 'Your API Key'
+summoner = 'Sadmo'
+champname = 'Teemo'
+
+ser = serial.Serial()
+ser.port = '/dev/ttyACM0' #Arduino serial port
+ser.baudrate = 9600
+ser.timeout = 10
+i = 0
+
+try:
+    ser.open()
+except:
+    print("Serial port not available")
+    exit()
+
+
+def generate_message(message1, message2):
+    blank = ' '
+    #top row message
+    if (len(message1) < 16) and (len(message1) % 2 == 0):
+        leftside = rightside =(16-len(message1))/2
+        #construct message
+        message = blank*int(leftside) + message1 + blank*int(rightside)
+    elif (len(message1) < 16) and (len(message1) % 2 != 0):
+        leftside = rightside =(15-len(message1))/2
+        #construct message
+        message = ' ' + blank*int(leftside) + message1 + blank*int(rightside)
+    elif (len(message1) == 16):
+        message = message1
+
+    #bottom row message
+    if (len(message2) < 16) and (len(message2) % 2 == 0):
+        leftside = rightside =(16-len(message2))/2
+        #construct message
+        message = message + blank*int(leftside) + message2 + blank*int(rightside)
+    elif (len(message2) < 16) and (len(message2) % 2 != 0):
+        leftside = rightside =(15-len(message2))/2
+        #construct message
+        message = message + ' ' + blank*int(leftside) + message2 + blank*int(rightside)
+    elif (len(message2) == 16):
+        message = message + message2
+    
+    return message
+
+while 1:
+    if i%900 == 0:
+        try:
+            masterylist = urlopen(f'https://oc1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner}?api_key={key}', timeout=5)
+            masterylist = masterylist.read().decode('utf-8')
+            summonerId = findall(r'\"id\":\"(.+?)\"',masterylist)
+            summonerId = summonerId[0]
+            level = findall(r'Level\":(.+?)}',masterylist)
+            level = level[0]
+
+            masterylist = urlopen(f'https://oc1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{summonerId}?api_key={key}', timeout=5)
+            # find string
+            masterypoint = findall("championId\":17.+?ints\":([0-9]+)",masterylist.read().decode('utf-8'))
+            #update label
+            masterypoint = masterypoint[0]
+
+
+        except:
+            masterypoint = 'error'
+            level = 'error'
+
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    current_date = now.strftime("%d/%m/%Y")
+
+    if (i%8 == 0) or(i%8 == 1):
+        ser.write(generate_message(current_time,current_date).encode())
+    elif(i%8 == 2) or(i%8 == 3):
+        ser.write(generate_message(current_time,f" {champname}Mastery").encode())
+    elif(i%8 == 4) or(i%8 == 5):
+        ser.write(generate_message(current_time,f"  {masterypoint} Points").encode())
+    elif(i%8 == 6) or(i%8 == 7):
+        ser.write(generate_message(current_time,f"Level {level}").encode())
+    
+    if i < 1798:
+        i += 1
+    else :
+        i = 0
+    sleep(1)
